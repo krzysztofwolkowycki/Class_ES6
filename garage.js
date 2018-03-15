11794794c775d40e2d9299991ae0eeef58d3a6b1
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2018-03-15 18:01:33 +0100

    push_splice_cars

--- garage.js
+++ garage.js
@@ -4,7 +4,6 @@ class samochod{
         this.model = model;
     }
 }
-
 class parking{
     constructor(lista){
         this.lista = lista;
@@ -12,11 +11,25 @@ class parking{
     }
   dodaj(samochod) {
      this.samochody.push(samochod);
-
+  }
+  usun(marka){
+      for(var i= 0; i<this.samochody.length; i++){
+          if(this.samochody[i].marka === marka){
+              this.samochody.splice(i,1);
+              return;
+          }
+      }
   }
 }
 
 parking = new parking("Lista samochodw");
 parking.dodaj(new samochod("Opel", "Vectra"));
+parking.dodaj(new samochod("Toyota", "Corolla"));
+parking.dodaj(new samochod("Fiat", "Tipo"));
+parking.dodaj(new samochod("Audi", "Q7"));
+parking.dodaj(new samochod("Opel", "Corsa"));
+console.log(parking);
+
+parking.usun("Opel");
 
 console.log(parking);
